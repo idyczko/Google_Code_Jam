9eac65040520e7445af06bb3577490d183775524
idyczko <igor.dyczko@gmail.com>
2018-03-06 21:46:20 +0100

    Finished 'Bathroom Stalls'

--- qualification_round_2017/toilet_stalls.cpp
+++ qualification_round_2017/toilet_stalls.cpp
@@ -7,18 +7,25 @@ using namespace std;
 void _main(long long N, long long K){
   map<long long, long long> Q;
   Q[N] = 1;
-  while(K > 0) {
-
+  while (K > 0) {
+    long long n = (--Q.end())->first;
+    long long k = (--Q.end())->second;
+    if(k >= K)
+      break;
+    Q[n/2] += k;
+    Q[(n-1)/2] += k;
+    K -= k;
+    Q.erase(--Q.end());
   }
-  printf("%lld %lld", ((--Q.end())->first)/2, ((--Q.end())->first -1)/2;
+  printf("%lld %lld\n", ((--Q.end())->first)/2, ((--Q.end())->first -1)/2);
 }
 
 int main() {
   int T, t;
   long long N, K;
-  scanf("%d", T);
-  t = 1;
-  while(t++ <= T) {
+  scanf("%d", &T);
+  t = 0;
+  while(++t <= T) {
     scanf("%lld %lld", &N, &K);
     printf("Case #%d: ", t);
     _main(N, K);
